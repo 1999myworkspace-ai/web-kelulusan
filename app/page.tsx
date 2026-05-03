0393a13c1c951a50d385e284853d9b65ee03e552
myworkspace <1999.my.workspace@gmail.com>
2026-05-03 22:06:28 +0700

    ganti logo

--- app/page.tsx
+++ app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
           <img
             src="/logo.png"
             alt="Logo SMK Yapan Indonesia"
-            className="mx-auto mb-3 h-16 w-16"
+            className="mx-auto mb-3 h-16 w-auto"
           />
           <h1 className="font-bold text-lg">
             HASIL KELULUSAN SMK YAPAN INDONESIA
