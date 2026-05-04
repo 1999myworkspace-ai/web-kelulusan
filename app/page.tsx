d0f37b7e73d6e1177627a4ff6a9a306b6e1d21a4
myworkspace <1999.my.workspace@gmail.com>
2026-05-04 12:27:15 +0700

    ganti data

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
         setTimeout(() => {
           alert("😆 TENANG... KAMU LULUS KOK!");
           setWarna("blue");
-        }, 20000);
+        }, 15000);
       }
     } finally {
       setLoading(false);
